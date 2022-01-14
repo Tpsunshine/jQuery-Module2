8537fbaafe540f34661c1345aaf5b9df0f8594c2
Tsumshine <tpdawsun@gmail.com>
2022-01-14 20:04:26 +0200

    jQuery Module 2

---
+++ Product.php
@@ -0,0 +1,9 @@
+# Get Product names
+$product_names_array = $_POST['product_name'];
+
+print_r($product_names_array);
+
+# Get Product amounts
+$amount_array = $_POST['amount'];
+
+print_r($amount_array);
